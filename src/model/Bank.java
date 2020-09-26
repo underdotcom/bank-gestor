3b7d24fa44ea11e7895e69d40a0ab8bfe6ab01b5
JhonSaldarriaga <jhonsaldarriagalopez@gmail.com>
2020-09-26 11:13:25 -0500

    Comments has been added

--- src/model/Bank.java
+++ src/model/Bank.java
@@ -20,23 +20,58 @@ public class Bank {
 	public Bank() {
 		dataBase = new HashTable<String, User>(ARRAY_SIZE);
 		deserters = new HashTable<String, Desertor>(ARRAY_SIZE/2);
+		
+		initializeTestCases();
 	}
 
+	private void initializeTestCases() {
+		
+	}
+	
 	public void addNewUser(User user) {
 		dataBase.add(user.getId(), user);
 	}
 	
 	public void addNewTurn(Turn turn) {
 		if(turn.getPriorityValue()>0) {
-			//addTurn
+			//addTurnPriority
 		}else {
-			//addTurn	
+			//addTurnCommon
 		}
+		
 		presentUsersList.add(dataBase.getValue(turn.getId()));
 	}
 	
-	public void cancelAccount(User user, String cancelationReason, LocalDate cancelationDate) {
+	public void attendCommon(int option) {
+		
+		
+		
+		//Elimina de la cola prioritaria
+	}
+	
+	public void attendSpecial(int option) {
+		
+		
+		
+		//Elimina de la cola 
+	}
+	
+	//Cualquier cliente
+	private void cancelAccount(User user, String cancelationReason, LocalDate cancelationDate) {
 		deserters.add(user.getId(), new Desertor(user, cancelationReason, cancelationDate));
 		dataBase.remove(user.getId());
 	}
+	
+	//Cuenta de ahorro
+	//retirar
+	//consignar
+	
+	
+	//Credit
+	//Pagar tarjeta
+	
+	
+	//sort merge
+	//heapSort
+	//Collectios
 }
